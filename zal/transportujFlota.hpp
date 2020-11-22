22744d536d9a92ec94f4b413738f8485c9c715f3
KinKossakow <kinga.kossakowska.stud@pw.edu.pl>
2020-11-22 18:31:58 +0000

    solved

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -2,8 +2,40 @@
 
 #include "Stocznia.hpp"
 
-unsigned int transportujFlota(unsigned int towar)
+unsigned int transportujFlota(unsigned int t)
 {
-    // Twoja implementacja tutaj
+  if (t > 0)
+  {
+    int iter;
+    iter = 0;
+    int sum;
+    sum =0;
+    int z;
+    z=0;
+    int tmp;
+    Stocznia stocznia{};
+    Statek* s1;
+   
+    while ( iter <100000 && sum < t)
+    {
+    s1 = stocznia();
+    tmp = s1->transportuj(); 
+    sum = sum + tmp;
+    iter = iter +1;
+    s1=dynamic_cast<Zaglowiec*>(s1);
+    if(s1)
+    {
+      z = z +1;
+    }
+    delete s1;
+    }
+
+   return z;
+  }
+
+  else
+  {
     return 0;
+  }
+
 }
